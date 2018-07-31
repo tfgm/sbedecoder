0f78ad40e6b53dd4d19709f2e99340ebce4d43e6
mhazard <matt.hazard@tradeforecaster.com>
2018-07-31 12:47:09 -0500

    Added python3.6 to languages in setup

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
     ]
 )
