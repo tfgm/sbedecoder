78cb3e960e6f449fb622681cc0e2814effc305b2
Bud Bach <bud@tradeforecaster.com>
2019-01-24 14:33:49 -0600

    Refactor tests to use pytest

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -1,218 +1,223 @@
 #!/usr/bin/env python
 
-import urllib
-import tempfile
-import os
-
-import nose
-from nose.tools import assert_equals
-from nose.tools import assert_is_instance
-from nose.tools import assert_is_none
-from nose.tools import assert_is_not_none
-from nose.tools import assert_true
+import pytest
 
 from mdp.orderbook import OrderBook
 
 
-class TestOrderBook:
+@pytest.fixture()
+def book():
+    book = OrderBook(9999, 3, 'TEST')
+    book.change(3, 'Offer', 8, 8, 8)
+    book.change(2, 'Offer', 7, 7, 7)
+    book.change(1, 'Offer', 6, 6, 6)
+    book.change(1, 'Bid', 3, 3, 3)
+    book.change(2, 'Bid', 2, 2, 2)
+    book.change(3, 'Bid', 1, 1, 1)
 
-    def setup(self):
-        self.book = OrderBook(9999, 3, 'TEST')
-        self.book.change(3, 'Offer', 8, 8, 8)
-        self.book.change(2, 'Offer', 7, 7, 7)
-        self.book.change(1, 'Offer', 6, 6, 6)
-        self.book.change(1, 'Bid', 3, 3, 3)
-        self.book.change(2, 'Bid', 2, 2, 2)
-        self.book.change(3, 'Bid', 1, 1, 1)
+    book.instrument_sequence = 0
 
-        self.book.instrument_sequence = 0
-
-    def teardown(self):
-        self.book = None
-
-    def test_adding_level_1_entries(self):
-        # def add(self, level, side, price, size, num_orders):
-        self.book.add(1, 'Offer', 5, 5, 5)
-        self.book.add(1, 'Bid', 4, 4, 4)
-
-        assert_equals(self.book.offers[0].price, 5)
-        assert_equals(self.book.offers[2].price, 7)
-
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[2].price, 2)
-
-    def test_adding_level_3_entries(self):
-        # def add(self, level, side, price, size, num_orders):
-        self.book.add(3, 'Offer', 9, 9, 9)
-        self.book.add(3, 'Bid', 0, 0, 0)
-
-        assert_equals(self.book.offers[0].price, 6)
-        assert_equals(self.book.offers[2].price, 9)
-
-        assert_equals(self.book.bids[0].price, 3)
-        assert_equals(self.book.bids[2].price, 0)
-
-    def test_deleting_level_1_entries(self):
-        # def add(self, level, side, price, size, num_orders):
-        self.book.delete(1, 'Offer')
-        self.book.delete(1, 'Bid')
-
-        assert_equals(self.book.offers[0].price, 7)
-        assert_is_none(self.book.offers[2].price)
-
-        assert_equals(self.book.bids[0].price, 2)
-        assert_is_none(self.book.bids[2].price)
-
-    def test_deleting_level_3_entries(self):
-        # def add(self, level, side, price, size, num_orders):
-        self.book.delete(3, 'Offer')
-        self.book.delete(3, 'Bid')
-
-        assert_equals(self.book.offers[0].price, 6)
-        assert_is_none(self.book.offers[2].price)
-
-        assert_equals(self.book.bids[0].price, 3)
-        assert_is_none(self.book.bids[2].price)
-
-    def test_change(self):
-        self.book.change(3, 'Offer', 8, 8, 8)
-        assert_equals(self.book.offers[2].price, 8)
-        assert_equals(self.book.offers[2].size, 8)
-        assert_equals(self.book.offers[2].num_orders, 8)
-
-        self.book.change(1, 'Bid', 4, 4, 4)
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[0].size, 4)
-        assert_equals(self.book.bids[0].num_orders, 4)
-
-#def update(self, sending_time,  received_time,  stream_sequence, instrument_sequence, level, md_entry_type, md_update_action, price, size, num_orders):
-    def test_update_book_change(self):
-
-        self.book.handle_update(101, 102, 1, 1, 3, 'Offer', 'Change', 8, 8, 8)
-
-        assert_equals(self.book.offers[0].price, 6)
-        assert_equals(self.book.offers[0].size, 6)
-        assert_equals(self.book.offers[0].num_orders, 6)
-
-        assert_equals(self.book.offers[2].price, 8)
-        assert_equals(self.book.offers[2].size, 8)
-        assert_equals(self.book.offers[2].num_orders, 8)
-
-        self.book.handle_update(101, 102, 2, 2, 1, 'Bid', 'Change', 4, 4, 4)
-
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[0].size, 4)
-        assert_equals(self.book.bids[0].num_orders, 4)
-
-        assert_equals(self.book.bids[2].price, 1)
-        assert_equals(self.book.bids[2].size, 1)
-        assert_equals(self.book.bids[2].num_orders, 1)
-
-    def test_update_book_add(self):
-
-        self.book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 5, 5, 5)
-
-        assert_equals(self.book.offers[0].price, 5)
-        assert_equals(self.book.offers[0].size, 5)
-        assert_equals(self.book.offers[0].num_orders, 5)
-
-        assert_equals(self.book.offers[2].price, 7)
-        assert_equals(self.book.offers[2].size, 7)
-        assert_equals(self.book.offers[2].num_orders, 7)
-
-        self.book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 4, 4, 4)
-
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[0].size, 4)
-        assert_equals(self.book.bids[0].num_orders, 4)
-
-        assert_equals(self.book.bids[2].price, 2)
-        assert_equals(self.book.bids[2].size, 2)
-        assert_equals(self.book.bids[2].num_orders, 2)
+    return book
 
 
-    def test_update_book_delete(self):
-        self.book.handle_update(101, 102, 1, 1, 1, 'Offer', 'Delete', None, None, None)
+def test_adding_level_1_entries(book):
+    # def add(self, level, side, price, size, num_orders):
+    book.add(1, 'Offer', 5, 5, 5)
+    book.add(1, 'Bid', 4, 4, 4)
 
-        assert_equals(self.book.offers[0].price, 7)
-        assert_equals(self.book.offers[0].size, 7)
-        assert_equals(self.book.offers[0].num_orders, 7)
-        assert_is_none(self.book.offers[2].price)
+    assert book.offers[0].price == 5
+    assert book.offers[2].price == 7
 
-        self.book.handle_update(101, 102, 2, 2, 1, 'Bid',  'Delete', None, None, None)
-        assert_equals(self.book.bids[0].price, 2)
-        assert_equals(self.book.bids[0].size, 2)
-        assert_equals(self.book.bids[0].num_orders, 2)
-        assert_is_none(self.book.bids[2].price)
+    assert book.bids[0].price == 4
+    assert book.bids[2].price == 2
 
-    def test_update_book_old_sequence(self):
-        self.book.instrument_sequence = 99
-        self.book.handle_update(101, 102, 100, 100, 1, 'Offer', 'New', 5, 5, 5)
-        self.book.handle_update(101, 102, 99, 99, 1, 'Offer', 'New', 999, 999, 999)
 
-        assert_equals(self.book.offers[0].price, 5)
-        assert_equals(self.book.offers[0].size, 5)
-        assert_equals(self.book.offers[0].num_orders, 5)
+def test_adding_level_3_entries(book):
+    # def add(self, level, side, price, size, num_orders):
+    book.add(3, 'Offer', 9, 9, 9)
+    book.add(3, 'Bid', 0, 0, 0)
 
-        assert_equals(self.book.offers[2].price, 7)
-        assert_equals(self.book.offers[2].size, 7)
-        assert_equals(self.book.offers[2].num_orders, 7)
+    assert book.offers[0].price == 6
+    assert book.offers[2].price == 9
 
-        self.book.instrument_sequence = 99
-        self.book.handle_update(101, 102, 100, 100, 1, 'Bid', 'New', 4, 4, 4)
-        self.book.handle_update(101, 102, 99, 99, 1, 'Bid', 'New', 999, 999, 999)
+    assert book.bids[0].price == 3
+    assert book.bids[2].price == 0
 
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[0].size, 4)
-        assert_equals(self.book.bids[0].num_orders, 4)
 
-        assert_equals(self.book.bids[2].price, 2)
-        assert_equals(self.book.bids[2].size, 2)
-        assert_equals(self.book.bids[2].num_orders, 2)
+def test_deleting_level_1_entries(book):
+    # def add(self, level, side, price, size, num_orders):
+    book.delete(1, 'Offer')
+    book.delete(1, 'Bid')
 
-    def test_update_book_duplicate_sequence(self):
-        self.book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 5, 5, 5)
-        self.book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 999, 999, 999)
+    assert book.offers[0].price == 7
+    assert book.offers[2].price is None
 
-        assert_equals(self.book.offers[0].price, 5)
-        assert_equals(self.book.offers[0].size, 5)
-        assert_equals(self.book.offers[0].num_orders, 5)
+    assert book.bids[0].price == 2
+    assert book.bids[2].price is None
 
-        assert_equals(self.book.offers[2].price, 7)
-        assert_equals(self.book.offers[2].size, 7)
-        assert_equals(self.book.offers[2].num_orders, 7)
 
-        self.book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 4, 4, 4)
-        self.book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 999, 999, 999)
+def test_deleting_level_3_entries(book):
+    # def add(self, level, side, price, size, num_orders):
+    book.delete(3, 'Offer')
+    book.delete(3, 'Bid')
 
-        assert_equals(self.book.bids[0].price, 4)
-        assert_equals(self.book.bids[0].size, 4)
-        assert_equals(self.book.bids[0].num_orders, 4)
+    assert book.offers[0].price == 6
+    assert book.offers[2].price is None
 
-        assert_equals(self.book.bids[2].price, 2)
-        assert_equals(self.book.bids[2].size, 2)
-        assert_equals(self.book.bids[2].num_orders, 2)
+    assert book.bids[0].price == 3
+    assert book.bids[2].price is None
 
-    def test_update_book_gapped(self):
-        return  # not ready for testing gaps until we handle more messages
-'''
-        self.book.handle_update(101, 102, 1, 1, 3, 'Offer', 'Change', 8, 8, 8)
-        self.book.handle_update(101, 102, 1, 2, 2, 'Offer', 'Change', 7, 7, 7)
-        self.book.handle_update(101, 102, 1, 3, 1, 'Offer', 'Change', 6, 6, 6)
-        self.book.handle_update(101, 102, 99, 99, 1, 'Offer', 'Change', 999, 999, 999)
 
-        assert_equals(self.book.offers[0].price, 999)
-        assert_equals(self.book.offers[0].size, 999)
-        assert_equals(self.book.offers[0].num_orders, 999)
+def test_change(book):
+    book.change(3, 'Offer', 8, 8, 8)
+    assert book.offers[2].price == 8
+    assert book.offers[2].size == 8
+    assert book.offers[2].num_orders == 8
 
-        assert_is_none(self.book.offers[1].price)
-        assert_is_none(self.book.offers[1].size)
-        assert_is_none(self.book.offers[1].num_orders)
+    book.change(1, 'Bid', 4, 4, 4)
+    assert book.bids[0].price == 4
+    assert book.bids[0].size == 4
+    assert book.bids[0].num_orders == 4
 
-        assert_is_none(self.book.bids[0].price)
-        assert_is_none(self.book.bids[0].size)
-        assert_is_none(self.book.bids[0].num_orders)
-'''
 
-if __name__ == '__main__':
-    nose.runmodule()
+def test_update_book_change(book):
+    book.handle_update(
+        sending_time=101,
+        received_time=102,
+        stream_sequence=1,
+        instrument_sequence=1,
+        level=3,
+        md_entry_type='Offer',
+        md_update_action='Change',
+        price=8,
+        size=8,
+        num_orders=8
+    )
+
+    assert book.offers[0].price == 6
+    assert book.offers[0].size == 6
+    assert book.offers[0].num_orders == 6
+
+    assert book.offers[2].price == 8
+    assert book.offers[2].size == 8
+    assert book.offers[2].num_orders == 8
+
+    book.handle_update(101, 102, 2, 2, 1, 'Bid', 'Change', 4, 4, 4)
+
+    assert book.bids[0].price == 4
+    assert book.bids[0].size == 4
+    assert book.bids[0].num_orders == 4
+
+    assert book.bids[2].price == 1
+    assert book.bids[2].size == 1
+    assert book.bids[2].num_orders == 1
+
+
+def test_update_book_add(book):
+    book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 5, 5, 5)
+
+    assert book.offers[0].price == 5
+    assert book.offers[0].size == 5
+    assert book.offers[0].num_orders == 5
+
+    assert book.offers[2].price == 7
+    assert book.offers[2].size == 7
+    assert book.offers[2].num_orders == 7
+
+    book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 4, 4, 4)
+
+    assert book.bids[0].price == 4
+    assert book.bids[0].size == 4
+    assert book.bids[0].num_orders == 4
+
+    assert book.bids[2].price == 2
+    assert book.bids[2].size == 2
+    assert book.bids[2].num_orders == 2
+
+
+def test_update_book_delete(book):
+    book.handle_update(101, 102, 1, 1, 1, 'Offer', 'Delete', None, None, None)
+
+    assert book.offers[0].price == 7
+    assert book.offers[0].size == 7
+    assert book.offers[0].num_orders == 7
+    assert book.offers[2].price is None
+
+    book.handle_update(101, 102, 2, 2, 1, 'Bid', 'Delete', None, None, None)
+    assert book.bids[0].price == 2
+    assert book.bids[0].size == 2
+    assert book.bids[0].num_orders == 2
+    assert book.bids[2].price is None
+
+
+def test_update_book_old_sequence(book):
+    book.instrument_sequence = 99
+    book.handle_update(101, 102, 100, 100, 1, 'Offer', 'New', 5, 5, 5)
+    book.handle_update(101, 102, 99, 99, 1, 'Offer', 'New', 999, 999, 999)
+
+    assert book.offers[0].price == 5
+    assert book.offers[0].size == 5
+    assert book.offers[0].num_orders == 5
+
+    assert book.offers[2].price == 7
+    assert book.offers[2].size == 7
+    assert book.offers[2].num_orders == 7
+
+    book.instrument_sequence = 99
+    book.handle_update(101, 102, 100, 100, 1, 'Bid', 'New', 4, 4, 4)
+    book.handle_update(101, 102, 99, 99, 1, 'Bid', 'New', 999, 999, 999)
+
+    assert book.bids[0].price == 4
+    assert book.bids[0].size == 4
+    assert book.bids[0].num_orders == 4
+
+    assert book.bids[2].price == 2
+    assert book.bids[2].size == 2
+    assert book.bids[2].num_orders == 2
+
+
+def test_update_book_duplicate_sequence(book):
+    book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 5, 5, 5)
+    book.handle_update(101, 102, 1, 1, 1, 'Offer', 'New', 999, 999, 999)
+
+    assert book.offers[0].price == 5
+    assert book.offers[0].size == 5
+    assert book.offers[0].num_orders == 5
+
+    assert book.offers[2].price == 7
+    assert book.offers[2].size == 7
+    assert book.offers[2].num_orders == 7
+
+    book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 4, 4, 4)
+    book.handle_update(101, 102, 2, 2, 1, 'Bid', 'New', 999, 999, 999)
+
+    assert book.bids[0].price == 4
+    assert book.bids[0].size == 4
+    assert book.bids[0].num_orders == 4
+
+    assert book.bids[2].price == 2
+    assert book.bids[2].size == 2
+    assert book.bids[2].num_orders == 2
+
+
+def test_update_book_gapped(book):
+    """
+    book.handle_update(101, 102, 1, 1, 3, 'Offer', 'Change', 8, 8, 8)
+    book.handle_update(101, 102, 1, 2, 2, 'Offer', 'Change', 7, 7, 7)
+    book.handle_update(101, 102, 1, 3, 1, 'Offer', 'Change', 6, 6, 6)
+    book.handle_update(101, 102, 99, 99, 1, 'Offer', 'Change', 999, 999, 999)
+
+    assert_equals(book.offers[0].price, 999)
+    assert_equals(book.offers[0].size, 999)
+    assert_equals(book.offers[0].num_orders, 999)
+
+    assert_is_none(book.offers[1].price)
+    assert_is_none(book.offers[1].size)
+    assert_is_none(book.offers[1].num_orders)
+
+    assert_is_none(book.bids[0].price)
+    assert_is_none(book.bids[0].size)
+    assert_is_none(book.bids[0].num_orders)
+    """
+
+    pass  # not ready for testing gaps until we handle more messages
--- tests/test_secdef.py
+++ tests/test_secdef.py
@@ -1,49 +1,38 @@
 #!/usr/bin/env python
-import six
-import tempfile
 import os
+import tempfile
 
-import nose
+import six
 from six.moves import urllib
-from nose.tools import assert_equals
-from nose.tools import assert_is_instance
-from nose.tools import assert_is_none
+import pytest
 
 from mdp.secdef import SecDef
 
-
-class TestSecDef:
-
-    secdef_filename = None
-    secdef_url = 'ftp://ftp.cmegroup.com/SBEFix/Production/secdef.dat.gz'
-    secdef = None
-
-    @classmethod
-    def setup_class(cls):
-        TestSecDef.secdef_filename = tempfile.NamedTemporaryFile().name
-        urllib.request.urlretrieve(TestSecDef.secdef_url, TestSecDef.secdef_filename)
-        TestSecDef.secdef = SecDef()
-        TestSecDef.secdef.load(TestSecDef.secdef_filename)
-
-    @classmethod
-    def teardown_class(cls):
-        os.remove(TestSecDef.secdef_filename)
-
-    def test_lookup_security_id_not_found(self):
-        print('Testing security lookup not found')
-        assert_is_none(TestSecDef.secdef.lookup_security_id(9999999))
-
-    def test_lookup_security_id_found(self):
-        # just grab a key from the dict so we have something to lookup
-        key_iter = six.iterkeys(TestSecDef.secdef.info)
-        key = six.next(key_iter)
-        value = TestSecDef.secdef.lookup_security_id(key)
-        # should get back a tuple with two items
-        assert_equals(len(value), 2)
-        symbol, depth = value
-        assert_is_instance(symbol, six.text_type)
-        assert_is_instance(depth, six.integer_types)
+secdef_url = 'ftp://ftp.cmegroup.com/SBEFix/Production/secdef.dat.gz'
 
 
-if __name__ == '__main__':
-    nose.runmodule()
+@pytest.fixture()
+def secdef():
+    secdef_filename = tempfile.NamedTemporaryFile().name
+    urllib.request.urlretrieve(secdef_url, secdef_filename)
+    secdef = SecDef()
+    secdef.load(secdef_filename)
+    os.remove(secdef_filename)
+    return secdef  # provide the fixture value
+
+
+def test_lookup_security_id_not_found(secdef):
+    print('Testing security lookup not found')
+    assert secdef.lookup_security_id(9999999) is None
+
+
+def test_lookup_security_id_found(secdef):
+    # just grab a key from the dict so we have something to lookup
+    key_iter = six.iterkeys(secdef.info)
+    key = six.next(key_iter)
+    value = secdef.lookup_security_id(key)
+    # should get back a tuple with two items
+    assert len(value) == 2
+    symbol, depth = value
+    assert isinstance(symbol, six.text_type)
+    assert isinstance(depth, six.integer_types)
